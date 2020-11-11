c141049af1126bcc0202eea43bd0a846130c23a5
Juan Esteban C <juan.carmona512@hotmail.com>
2020-11-10 20:56:15 -0500

    Adding coins fetch added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,25 +14,6 @@ const Profile = () => {
       body:<div></div>
   });
 
-  useEffect(() => {
-    const Url =
-      "https://private-anon-44244cc0a3-aerolabchallenge.apiary-proxy.com/user/me";
-    const headers = {
-      method: "GET",
-      headers: new Headers({
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmFhYzljZGI5NTIzZTAwMjA3ZTFmYzIiLCJpYXQiOjE2MDUwMjgzMDF9.AmLe0RxgByiXoIvSND0TFzRmZoN1DZQXFh2XAWt21bE",
-      }),
-    };
-    fetch(Url, headers)
-      .then((res) => res.json())
-      .then((res) => {
-        setUser(res);
-      });
-  }, []);
-
   const ShowHistory =()=>{
     setModal({
         body: <RedeemHistory></RedeemHistory>,
